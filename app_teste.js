d72dc64e9ef886851c8e98d53d5e5943cf49a049
cyvitor <cyvitorhugo@gmail.com>
2023-01-06 17:07:36 -0300

    testando funções anonimas

--- app_teste.js
+++ app_teste.js
@@ -1,2 +1,20 @@
-console.log("Testando node");
-console.log("Testando node2");
+
+function log() {
+    console.log("Testando node");
+}
+
+log();
+
+function log2(men) {
+    console.log("--> " + men);
+}
+
+
+let add = function (a, b){
+	return a + b;
+};
+
+let add2 = (a, b) => a + b;
+
+log2(add2(2,2));
+
