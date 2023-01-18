e40cf52dc3ea2fa310f6b4bb6594816a26060df3
cyvitor <cyvitorhugo@gmail.com>
2023-01-18 09:54:54 -0300

    redirectPage funcionando

--- frontend/src/pages/RedirectPage/index.js
+++ frontend/src/pages/RedirectPage/index.js
@@ -1,13 +1,45 @@
 import React from "react";
+import { Container } from "react-bootstrap";
+import Header from "../../components/Header";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import ShortenerService from "../../services/shortenerService";
 
 class RedirectPage extends React.Component {
-    constructor(props){
+    constructor(props) {
         super(props);
+
+        this.state = {
+            isLoading: false,
+            url: '',
+            errorMessage: '',
+        }
     }
 
-    render(){
-        return(
-            <p>Pitu RedirectPage</p>
+    async componentDidMount() {
+        const { code } = this.props.match.params;
+
+        try {
+            const service = new ShortenerService();
+            const { url } = await service.getLink(code);
+            window.location = url;
+        } catch (error) {
+            this.setState({ isLoading: false, errorMessage: 'ERROR, deu ruim ' + error })
+        }
+    }
+
+    render() {
+        const { errorMessage } = this.state;
+        return (
+            <Container>
+                <Header title="Pitu - encutrador de link">RedirectPage</Header>
+                {errorMessage ? (
+                    <Container className="text-center">
+                        <FontAwesomeIcon size="3x" color="#f8d7da" icon="exclamation-triangle" />
+                        <p className="m-3">{errorMessage}</p>
+                        <a className="btn btn-primary" href="/" >Encontar uma URL</a>
+                    </Container>
+                ) : (<><p className="text-center">Redirecionando....</p></>)}
+            </Container>
         )
     }
 
